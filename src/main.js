62489bf110ed12ca708f41554e403fc7bb28bee1
Kenneth Du <kennethdu3@gmail.com>
2018-10-19 09:06:05 -0700

    add pseudo code for main.js for user input

--- src/main.js
+++ src/main.js
@@ -2,8 +2,36 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import { getIssue, getDoctor } from './doctor.js';
 // const Promise = require('es6-promise').Promise;
 
 $(document).ready(function() {
+    $("#nameForm").submit(function(event){
+        event.preventDefault();
+        $("nameResults").text("");
+        $("issueResults").text("");
+        let name = $("name").val();
+        // let promise1 = getDoctor(name);
+
+        // promise1.then(function(response){
+        //     let body = JSON.parse(response);
+        //     let nameList = body;
+
+        });
+    });
+
+    $("#nameForm").submit(function (event) {
+        event.preventDefault();
+        $("nameResults").text("");
+        $("issueResults").text("");
+        let issue = $("issue").val();
+        // let promise2 = getIssue(issue);
+
+        // promise2.then(function (response) {
+        //     let body = JSON.parse(response);
+        //     let nameList = body;
+
+        // });
+    });
 
 });
