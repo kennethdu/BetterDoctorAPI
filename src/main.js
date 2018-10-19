73e63bb22fb2f13381aaac0fabcfcb649710d898
Kenneth Du <kennethdu3@gmail.com>
2018-10-19 12:24:44 -0700

    trying to return doctor information blocked out search by query at the moment

--- src/main.js
+++ src/main.js
@@ -1,37 +1,44 @@
+/* eslint-disable no-unused-vars */
+
+
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import { getIssue, getDoctor } from './doctor.js';
-// const Promise = require('es6-promise').Promise;
+const Promise = require('es6-promise').Promise;
 
 $(document).ready(function() {
     $("#nameForm").submit(function(event){
         event.preventDefault();
-        $("nameResults").text("");
-        $("issueResults").text("");
-        let name = $("#name").val();
-        // let promise1 = getDoctor(name);
+        $("#nameResults").text("");
+        $("#issueResults").text("");
+        let firstName = $("#firstName").val();
+        let lastName = $("#lastName").val();
+        let promise1 = getDoctor(firstName, lastName);
 
-        // promise1.then(function(response){
-        //     let body = JSON.parse(response);
-        //     let nameList = body;
-
+        promise1.then(function(response){
+            let body = JSON.parse(response);
+            if(body.data.length > 0){
+            }else{
+                $("#nameResult").append("There are no doctors matching your criteria");
+                }
+            });
         });
     });
 
-    $("#issueForm").submit(function (event) {
-        event.preventDefault();
-        $("nameResults").text("");
-        $("issueResults").text("");
-        let issue = $("#issue").val();
-        // let promise2 = getIssue(issue);
+    // $("#issueForm").submit(function (event) {
+    //     event.preventDefault();
+    //     $("#nameResults").text("");
+    //     $("#issueResults").text("");
+    //     let issue = $("#issue").val();
+    //     // let promise2 = getIssue(issue);
 
-        // promise2.then(function (response) {
-        //     let body = JSON.parse(response);
-        //     let nameList = body;
+    //     // promise2.then(function (response) {
+    //     //     let body = JSON.parse(response);
+    //     //     let nameList = body;
 
-        // });
-    });
+    //     // });
+    // });
 
-});
+// });
