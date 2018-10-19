b403c98916da33368b446012debf3be6b89d093d
Kenneth Du <kennethdu3@gmail.com>
2018-10-19 13:17:55 -0700

    add env API key in url for api

--- src/doctor.js
+++ src/doctor.js
@@ -1,7 +1,8 @@
-export function getDoctor(name){
+export function getDoctor(firstName, lastName){
     return new Promise(function(resolve, reject){
         let request = new XMLHttpRequest();
-        let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&user_location=37.773%2C-122.413&skip=0&limit=10&name=${name}&user_key=1202d56e7b58243cff409a6a1360eeab`
+        let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=wa-seattle&skip=0&limit=10&first_name=${firstName}&last_name=${lastName}&user_key=${process.env.API_KEY}`
+        console.log(url);
         request.onload = function() {
             if(this.status === 200){
                 resolve(request.response);
@@ -17,7 +18,7 @@ export function getDoctor(name){
 export function getIssue(issue){
     return new Promise(function (resolve, reject) {
         let request = new XMLHttpRequest();
-        let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&user_location=37.773%2C-122.413&skip=0&limit=10&query=${issue}&user_key=1202d56e7b58243cff409a6a1360eeab`
+        let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=wa-seattle&skip=0&limit=10&query=${issue}&user_key=${process.env.API_KEY}`
         request.onload = function () {
             if (this.status === 200) {
                 resolve(request.response);
@@ -28,5 +29,4 @@ export function getIssue(issue){
         request.open("GET", url, true);
         request.send();
     });
-
 }
