43d317ab1ed3b98487e425ec3c6be9f6aa06a02f
Kenneth Du <kennethdu3@gmail.com>
2018-10-19 13:33:02 -0700

    return proper data from the API by searching by medical issue. Returns name, address, phone number, website, and whether or not the doctor is accepting new patients

--- src/main.js
+++ src/main.js
@@ -32,7 +32,7 @@ $(document).ready(function() {
                     <h5>${body.data[i].practices[0].accepts_new_patients}</h5>`);
                 }
             }else{
-                $("#nameResult").append("There are no doctors matching your criteria");
+                $("#nameResult").append("There are no doctors matching your criteria, try again!");
                 }
             });
         });
@@ -49,7 +49,16 @@ $(document).ready(function() {
             let body = JSON.parse(response);
             if (body.data.length > 0) {
                 for (let i = 0; i < body.data.length; i++) {
-                    $("#issueResults").append(`<h1>${body.data[i].practices[0].name}</h1>`);
+                    $("#issueResults").append(`<h1>${body.data[i].practices[0].name}</h1>
+                    <h3>Address:</h3>
+                    <h5>${body.data[i].practices[0].visit_address.street}, ${body.data[i].practices[0].visit_address.zip}</h5>
+                    <h5>${body.data[i].practices[0].visit_address.city}, ${body.data[i].practices[0].visit_address.state}</h5>
+                    <h3>Phone Number:</h3>
+                    <h5>${body.data[i].practices[0].phones[0].number}</h5>
+                    <h3>Website</h3>
+                    <h5>${body.data[i].practices[0].website}</h5>
+                    <h3>Is the doctor accepting new patients(true/false)?</h3>
+                    <h5>${body.data[i].practices[0].accepts_new_patients}</h5>`);
                 }
             } else {
                 $("#issueResults").append("There are no doctors matching your criteria, try again!");
