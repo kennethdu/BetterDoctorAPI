e21e389edcebcf6cbc7503fde2ea38e7e8905550
Kenneth Du <kennethdu3@gmail.com>
2018-10-19 09:22:41 -0700

    add code for getDoctor() and getIssue()

--- src/doctor.js
+++ src/doctor.js
@@ -1,7 +1,32 @@
 export function getDoctor(name){
-
-};
+    return new Promise(function(resolve, reject){
+        let request = new XMLHttpRequest();
+        let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&user_location=37.773%2C-122.413&skip=0&limit=10&name=${name}&user_key=1202d56e7b58243cff409a6a1360eeab`
+        request.onload = function() {
+            if(this.status === 200){
+                resolve(request.response);
+            }else{
+                reject(Error(request.statusText));
+            }
+        };
+        request.open("GET", url, true);
+        request.send();
+    });
+}
 
 export function getIssue(issue){
+    return new Promise(function (resolve, reject) {
+        let request = new XMLHttpRequest();
+        let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&user_location=37.773%2C-122.413&skip=0&limit=10&query=${issue}&user_key=1202d56e7b58243cff409a6a1360eeab`
+        request.onload = function () {
+            if (this.status === 200) {
+                resolve(request.response);
+            } else {
+                reject(Error(request.statusText));
+            }
+        };
+        request.open("GET", url, true);
+        request.send();
+    });
 
-};
+}
